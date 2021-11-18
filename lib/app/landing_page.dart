6d120ddfc49707855d4a4217847e10e1dcfd922d
Momshad Dinury <md.dinury@gmail.com>
2021-11-18 15:19:20 +0600

    Sign In -> Sign out flow complete

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tracker/app/sign_in/sign_in_page.dart';
+
+import 'home_page.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateUser(FirebaseAuth.instance.currentUser);
+  }
+
+  void _updateUser(User? user) {
+    setState(() {
+      _user = user;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_user == null) {
+      return SignInPage(
+        onSignIn: (user) => _updateUser(user),
+      );
+    }
+    return HomePage(
+      onSignOut: () => _updateUser(null),
+    );
+  }
+}
