dfb2f9819c6d9e70bc2582a3bfa3472c6a84bb32
Momshad Dinury <md.dinury@gmail.com>
2021-11-18 13:16:01 +0600

    Firebase anonymous login feature

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker/app/sign_in/social_sign_in_button.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
+
+  Future<void> _signInAnonymously() async {
+    try {
+      final userCredentials = await FirebaseAuth.instance.signInAnonymously();
+      print('${userCredentials.user?.uid}');
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +79,7 @@ class SignInPage extends StatelessWidget {
             text: "Go anonymous",
             textColor: Colors.black,
             color: Colors.lime[300],
-            onPressed: () {},
+            onPressed: _signInAnonymously,
           ),
         ],
       ),
