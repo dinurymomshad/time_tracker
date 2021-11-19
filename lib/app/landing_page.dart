7b9e31c82687a4cc4d67f1746f00917afa9a93df
Momshad Dinury <md.dinury@gmail.com>
2021-11-19 11:17:46 +0600

    Lifting state up (Has issue with null value in currentUser())

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,11 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker/services/auth.dart';
 
 import 'home_page.dart';
 
 class LandingPage extends StatefulWidget {
-  const LandingPage({Key? key}) : super(key: key);
+  final AuthBase? auth;
+
+  const LandingPage({
+    Key? key,
+    @required this.auth,
+  }) : super(key: key);
 
   @override
   _LandingPageState createState() => _LandingPageState();
@@ -17,7 +23,7 @@ class _LandingPageState extends State<LandingPage> {
   @override
   void initState() {
     super.initState();
-    _updateUser(FirebaseAuth.instance.currentUser);
+    //  _updateUser(widget.auth!.currentUser);
   }
 
   void _updateUser(User? user) {
@@ -30,10 +36,12 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     if (_user == null) {
       return SignInPage(
+        auth: widget.auth,
         onSignIn: (user) => _updateUser(user),
       );
     }
     return HomePage(
+      auth: widget.auth,
       onSignOut: () => _updateUser(null),
     );
   }
